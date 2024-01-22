e2c530a100917005dcbfce9dbabcd98427c2ac35
tjwjddn980117 <tjwjddn15584@naver.com>
2024-01-22 11:19:05 +0900

    conf

--- models/utils/conf.py
+++ models/utils/conf.py
@@ -10,10 +10,10 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 mydir = os.getcwd()
 #DATA_PATH = mydir + '\\content'
 DATA_PATH = 'C:\\Users\\qowor\\Desktop\\open'
-SAVE_ORIGIN_PATH = DATA_PATH+'\\origin'
-SAVE_AGUMENT_PATH = DATA_PATH+'\\augment'
-TRAIN_DF = pd.read_csv(DATA_PATH+'\\train.csv')
-TRAIN_DF = pd.read_csv(DATA_PATH+'\\test.csv')
+SAVE_ORIGIN_PATH = os.path.join(DATA_PATH, 'origin')
+SAVE_AGUMENT_PATH = os.path.join(DATA_PATH, 'augment')
+TRAIN_DF = pd.read_csv(os.path.join(DATA_PATH, 'train'))
+TRAIN_DF = pd.read_csv(os.path.join(DATA_PATH, 'test'))
 
 # parameter about model.
 BATCH_SIZE = 32
