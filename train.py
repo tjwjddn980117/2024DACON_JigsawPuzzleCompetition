183119645ee9901af1626a487e57962015f12ef5
tjwjddn980117 <tjwjddn15584@naver.com>
2024-01-22 11:18:14 +0900

    train

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import time
+
 from torch import nn, optim
 from torch.optim import Adam
 
 from models.model.JigsawNet import JIGSAW_NET
 from models.utils.conf import *
-from models.datasets.dataset import DATA_LOADER
+from models.datasets.dataset import TRAIN_DATA_LOADER, TEST_DATA_LOADER
 
 model = JIGSAW_NET(3)
 TRUE_LABEL = list(range(16))
@@ -40,3 +42,19 @@ def evaluation(model, datasets, criterion):
     with torch.no_grad():
         for i, batch in enumerate(datasets):
             output = model(batch)
+            output_reshape = output.contiguous().view(-1, output.shape[-1])
+
+            loss = criterion(output, output_reshape)
+            loss.backward()
+
+            epoch_loss += loss.item()
+        
+    return epoch_loss / len(datasets)
+
+def run(total_epoch, best_loss):
+    train_losses, test_losses = [], []
+    for step in range(total_epoch):
+        start_time = time.time()
+        train_loss = train(model, TRAIN_DATA_LOADER, optimizer, criterion)
+        valid_loss = evaluation(model, TEST_DATA_LOADER, criterion)
+        end_time = time.time()
